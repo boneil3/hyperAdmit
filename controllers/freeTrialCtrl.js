983db3b2f8f1bba71b8281eca8e069c6c565a606
Brendan O'Neil <brendanroneil@gmail.com>
2015-04-23 18:52:46 -0400

    beep boop fag

--- controllers/freeTrialCtrl.js
+++ controllers/freeTrialCtrl.js
@@ -1,5 +1,31 @@
 /**
  * Created by brendan on 4/23/15.
  */
-hyperApp.controller('freeTrialCtrl', function ($location, $scope, $window, $modal, $modalInstance, GApi, sessionService) {
+
+hyperApp.controller('freeTrialCtrl', function ($location, $scope, $window, $modalInstance, GApi, sessionService) {
+
+    var vm = this;
+        vm.user = {};
+
+    vm.submitFreeTrial = function (user, school_type) {
+        GApi.execute('centralparkedu', 'hyperadmit.free_trial_signup', {
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'email': user.email,
+            'phone': user.phone,
+            'school_type': school_type
+        }).then(function (response) {
+            if (response.code >= 400) {
+                window.alert('Error Processing Request!');
+            }
+            else {
+                sessionService.set('email', response.email);
+                $location.path('/finishTrial');
+            }
+        })
+    };
+
+    $scope.cancel = function() {
+        $modalInstance.dismiss('closed');
+    };
 });
--- controllers/hyperApp.js
+++ controllers/hyperApp.js
@@ -33,6 +33,11 @@ hyperApp.config(['$routeProvider', function ($routeProvider) {
             controller: 'aboutCtrl',
             controllerAs: 'vm'
         })
+        .when('/freeTrial', {
+            templateUrl: 'freeTrial',
+            controller: 'freeTrialCtrl',
+            controllerAs: 'vm'
+        })
         .otherwise({
             redirectTo: '/home'
         });
@@ -91,7 +96,7 @@ hyperApp.run(['$window', 'GApi', '$rootScope', '$location', 'loginService', func
     GApi.load('oauth2', 'v2');
 
     $rootScope.$on('$routeChangeStart', function () {
-        if ($location.path() !== '/login' && !loginService.isLogged() && $location.path() !== '/signup' && $location.path() !== '/reset' && $location.path() !== '/home' && $location.path() !== '/about') {
+        if ($location.path() !== '/login' && !loginService.isLogged() && $location.path() !== '/signup' && $location.path() !== '/reset' && $location.path() !== '/home' && $location.path() !== '/about' && location.path() !== '/freeTrial') {
             $location.path('/home');
         }
     });
